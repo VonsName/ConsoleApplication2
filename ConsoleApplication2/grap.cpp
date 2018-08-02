2ce72e80a71b3694a7bc7771ee3dddfa71267743
fengjianglin <fengjianglin6817@dingtalk.com>
2018-08-02 09:33:30 +0800

    update

--- ConsoleApplication2/grap.cpp
+++ ConsoleApplication2/grap.cpp
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <graphics.h>
 int main() {
+	//初始化图形窗口
 	initgraph(640, 480);
+	//设置背景颜色
+	setbkcolor(BLUE);
+	//使用背景色清空屏幕
+	cleardevice();
+	//设置绘图颜色
+	setcolor(RED);
+	//画圆
+	circle(50, 60, 50);
+	//阻塞
 	getchar();
+	//关闭图形窗口
 	closegraph();
 	return 0;
 }
